1086761a4e80679deb59c55a5c0b9c6cf647bc91
KaragozE <karagoz.eray@gmail.com>
2018-11-27 11:10:45 +0300

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            while (flag == 0)
+            {
+                Console.Write("Kullanıcı adı giriniz:");
+                string kAdi = Console.ReadLine();
+                Console.Write("Şifre Giriniz:");
+                string pwd = Console.ReadLine();
+
+                if (kAdi == "admin" && pwd == "123")
+                {
+                    Console.WriteLine("Hoşgeldiniz");
+                    flag = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış kullanıcı adı ya da şifre");
+                }
+            }
+            Console.WriteLine("Portal Eray");
+            Console.ReadKey();
+        }
+    }
+}
